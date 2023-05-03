61beddc1b198ab4f7ed63d02a13978a7e66fe41e
thomga <gabriel.thom02@gmail.com>
2023-05-03 12:20:31 -0300

    chore: add rollux testnet

--- src/buildList.js
+++ src/buildList.js
@@ -1,22 +1,13 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
-const optimism = require("./tokens/optimism.json");
-const celo = require("./tokens/celo.json");
-const arbitrum = require("./tokens/arbitrum.json");
-const bnb = require("./tokens/bnb.json");
+const rollux = require("./tokens/rollux.json");
+
 
 const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   const l1List = {
-    name: "Uniswap Labs Default",
+    name: "Pegasys Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -24,9 +15,9 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo, ...arbitrum, ...bnb]
+    logoURI: "https://raw.githubusercontent.com/pollum-io/pegasys-tokenlists/master/logos/0xd3e822f3ef011Ca5f17D82C956D952D8d7C3A1BB/logo.png",
+    keywords: ["pegasys", "default"],
+    tokens: [...rollux]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
